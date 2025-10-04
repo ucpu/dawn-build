5f76e748b9991607775da922e9b57dc073a1d16f
Tomáš Malý <malytomas@users.noreply.github.com>
2025-10-04 23:00:58 +0200

    fix cmake (3)

--- DawnConfig.cmake
+++ DawnConfig.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 3.31)
 
-add_library(dawn_headers INTERFACE IMPORTED)
+add_library(dawn_headers INTERFACE IMPORTED GLOBAL)
 target_include_directories(dawn_headers INTERFACE "${CMAKE_CURRENT_LIST_DIR}/include")
 
-add_library(dawn_static STATIC IMPORTED)
+add_library(dawn_static STATIC IMPORTED GLOBAL)
 target_link_libraries(dawn_static INTERFACE dawn_headers)
 if(WIN32)
 	set_target_properties(dawn_static PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/lib/webgpu_dawn.lib")
