32577d86da76bf54c6b57af31f4337087ae5e815
Tomáš Malý <malytomas@users.noreply.github.com>
2025-10-04 04:34:22 +0200

    workflow

--- run.sh
+++ run.sh
@@ -5,14 +5,21 @@ root_dir=`pwd`
 #sed -i 's/\bSTATIC\b/OBJECT/g' dawn/dawn/src/tint/CMakeLists.txt
 #sed -i 's/\btint_add_target\b/tint_add_target_override/g' dawn/dawn/src/tint/CMakeLists.txt
 
-mkdir build
-cd build
+suffix="_linux"
+paralel="-- -j8"
+if [ "$OSTYPE" = "msys" ]; then
+	suffix="_windows"
+	paralel="--parallel 8"
+fi
+
+mkdir build${suffix}
+cd build${suffix}
 echo "configuring"
 cmake -DCMAKE_BUILD_TYPE=RELEASE ..
 echo "building"
-cmake --build . --config Release
+cmake --build . --config Release ${paralel}
 echo "installing"
-cmake --install . --config Release --prefix "${root_dir}/install"
+cmake --install . --config Release --prefix "${root_dir}/install${suffix}"
 echo "done"
 cd "$root_dir"
 
